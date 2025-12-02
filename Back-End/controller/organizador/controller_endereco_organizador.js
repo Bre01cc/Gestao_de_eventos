61acb902b52ca0ae2c3bb4b743166de31da1dafc
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-12-02 08:40:45 -0300

    feat: implementando controller de endereco de organizador

--- Back-End/controller/organizador/controller_endereco_organizador.js
+++ Back-End/controller/organizador/controller_endereco_organizador.js
@@ -99,3 +99,140 @@ const listOrganizerAdresessByOrganizerID = async function(organizerID){
         return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500(erro interno)
     }
 }
+
+const setOrganizerAddress = async function(address, contentType){
+    //Criando um objeto para as mensagens
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+    
+     try{
+        //Validação do tipo de conteúdo da requisição (Obrigatório ser um JSON)
+        if(String(contentType).toUpperCase() == 'APPLICATION/JSON'){
+            //Guarda o resultado da validação de dados do Organizador
+            let validate = await validateOrganizerAddress(address)
+            if(!validate){
+                let resultAddress = await organizer_addressDAO.insertOrganizerAddress(address)
+
+                if(resultAddress){
+
+                    let lastID = await organizer_addressDAO.getLastId()
+
+                    if(lastID){
+                        address.id = lastID
+
+                        MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_CREATED_ITEM.status
+                        MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_CREATED_ITEM.status_code
+                        MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_CREATED_ITEM.message
+                        MESSAGES.DEFAULT_HEADER.items = address
+
+                        return MESSAGES.DEFAULT_HEADER
+
+                    }else{
+                        return MESSAGES.ERROR_INTERNAL_SERVER_MODEL
+                    }
+
+                }else{
+                    return MESSAGES.ERROR_INTERNAL_SERVER_MODEL
+                }
+
+            }else{
+                return validate
+            }
+
+        }else{
+            return MESSAGES.ERROR_CONTENT_TYPE
+        }
+
+    }catch(error){
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER
+    }
+}
+
+const setUpdateOrganizerAddress = async function(address, id, contentType){
+    //Criando um objeto para as mensagens
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+    
+    try{
+        //Validação do tipo de conteúdo da requisição (Obrigatório ser um JSON)
+        if(String(contentType).toUpperCase() == 'APPLICATION/JSON'){
+            //Guarda o resultado da validação de dados do Organizador
+            let validate = await validateOrganizer(address)
+            if(!validate){
+    
+                let validateID = await listOrganizerByID(id)
+                if(validateID.status_code == 200){
+    
+                    address.id = Number(id)
+    
+                    let resultAddress = await organizerDAO.updateOrganizaer(address)
+    
+                        if(resultAddress){
+    
+                            address.id = lastId
+    
+                            MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_CREATED_ITEM.status
+                            MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_CREATED_ITEM.status_code
+                            MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_CREATED_ITEM.message
+                            MESSAGES.DEFAULT_HEADER.items = address
+    
+                            return MESSAGES.DEFAULT_HEADER //201
+    
+                        }else{
+                        return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+                        }
+    
+                    }else{
+                        return validateID //Referente a validação da função de busca por ID poderá retornar (400 | 404 | 500)
+                    }
+    
+            }else{
+                return validate //400 Referente a  validação dos dados
+            }
+        }else{
+            return MESSAGES.ERROR_CONTENT_TYPE //415
+        }
+    
+    }catch(error){
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+
+
+
+
+// Função para validar todos os dados do endereco enviado
+const validateOrganizerAddress = async function(address){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    //Validações de todas entradas de dados
+    if(address.cep == '' || address.cep == undefined || address.cep == null || address.cep.length > 8 ){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [cep inválido]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+
+    }else if(address.cidade == '' || address.cidade == undefined || address.cidade == null || address.cidade.length > 200){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [cidade incorreta]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+
+    }else if(address.bairro == '' || address.bairro == undefined || address.bairro == null || address.bairro.length != 14){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [bairro incorreto]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+    }
+    else if(address.numero == '' || address.numero == undefined || address.numero == null || address.numero.length > 50){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [numero incorreto]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+
+    }else if(address.endereco == '' || address.endereco == undefined || address.endereco == null || address.endereco.length > 200){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [endereco incorreto]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+
+    }else if(address.id_uf == '' || address.id_uf == undefined || address.id_uf == null || !isNaN(address.id_uf)){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [id_uf incorreto]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+
+    }else if(address.id_organizador == '' || address.id_organizador == undefined || address.id_organizador == null || !isNaN(address.id_organizador)){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [id_organizador incorreto]' 
+        return MESSAGES.ERROR_REQUIRED_FIELDS //400
+    }
+
+}
+
+
