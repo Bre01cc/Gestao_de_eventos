ec8f8710372cef215626fecbe52f62ab378a4548
WESLEI SANTOS BEZERRA <25133277@cfp127aluno.local>
2025-12-03 10:18:49 -0300

    Inserts atualizados

--- Banco/Inserts/Inserts.sql
+++ Banco/Inserts/Inserts.sql
@@ -1,4 +1,4 @@
--- INSSERT DE ESTADOS(UF) --
+-- INSERT DE ESTADOS(UF) --
 INSERT INTO tb_uf (sigla) VALUES
 ('AC'), ('AL'), ('AP'), ('AM'), ('BA'), ('CE'),
 ('DF'), ('ES'), ('GO'), ('MA'), ('MT'), ('MS'),
@@ -27,12 +27,82 @@ VALUES
 INSERT INTO tb_endereco_organizador
 (cep, cidade, bairro, numero, logradouro, id_uf, id_organizador)
 VALUES
-('01001000', 'São Paulo', 'Centro', '150', 'Av. São João', 25, 1), -- Music Live
-('20040002', 'Rio de Janeiro', 'Centro', '210', 'Rua da Quitanda', 19, 2), -- Cosplay World
-('30110015', 'Belo Horizonte', 'Savassi', '500', 'Rua Pernambuco', 13, 3), -- Game On Expo
-('80010020', 'Curitiba', 'Batel', '820', 'Av. Batel', 16, 4); -- Tech Future
+('01001000', 'São Paulo', 'Centro', '150', 'Av. São João', 25, 1),
+('20040002', 'Rio de Janeiro', 'Centro', '210', 'Rua da Quitanda', 19, 2),
+('30110015', 'Belo Horizonte', 'Savassi', '500', 'Rua Pernambuco', 13, 3),
+('80010020', 'Curitiba', 'Batel', '820', 'Av. Batel', 16, 4);
 -----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE ENDEREÇO DOS PARTICIPANTES --
+INSERT INTO tb_endereco_participante
+(cep, cidade, bairro, numero, logradouro, id_uf, id_participante)
+VALUES
+('01001000', 'Itapevi', 'Suburbano', '2150', 'Rua das Palmeiras', 25, 3),
+('22041001', 'Jandira', 'Centro', '450', 'Avenida Brasil', 25, 2),
+('30140120', 'Itapevi', 'Rosemary ', '374', 'Rua Serra do Paracaima ', 25, 1),
+('01503000', 'São Paulo', 'Liberdade', '55', 'Rua da Liberdade', 1, 4),
+('51011020', 'Recife', 'Boa Viagem', '999', 'Rua do Sol', 4, 5);
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE CATEGORIAS --
+INSERT INTO tb_categoria (nome) VALUES
+('Música'), ('Tecnologia'), ('Games'), ('Cosplay'),
+('Teatro'), ('Esportes'), ('Palestra'), ('Festival'),
+('Feira'), ('Cultura'), ('Bem-estar');
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE STATUS EVENTOS --
+INSERT INTO tb_status_evento (nome)
+VALUES 
+('Ativo'),
+('Cancelado'),
+('Encerrado'),
+('Esgotado');
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DO ENDEREÇO DO EVENTO  --
+INSERT INTO tb_endereco_evento
+(cep, logradouro, cidade, bairro, numero, id_uf)
+VALUES
+('05653070', 'Praça Roberto Gomes Pedrosa', 'São Paulo', 'Morumbi', '1', 25);
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE EVENTOS --
+INSERT INTO tb_evento
+(nome, descricao, capa_url, data, id_organizador, id_status_evento, id_endereco_evento)
+VALUES
+(
+    'Rock Sunset Festival',
+    'Show ao ar livre com diversas bandas de rock, food trucks e atividades culturais.',
+    'https://lh3.googleusercontent.com/gg-dl/ABS2GSkMgDXu5seSL_lC4hPk9vO0jsq3zhUvS39bA3qGdqYYchRgqntj74yLVMqwce2OLEXjeudY70zknjQ7RtL-yC4OgRJxZQa54zelPMyxOOJVrMBwolbWq_gu7FY5Oi_8FOns1125WLgAts0-KRsiHFw7TT4ON8rT7sCuZ45Fvar8kxBp=s1024-rj',
+    '2025-12-31',
+    1,
+    1,
+    1
+);
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE SETORES DO EVENTOS --
+INSERT INTO tb_setor (nome, capacidade, id_evento)
+VALUES 
+('Pista', 200, 4),
+('VIP', 50, 4),
+('Backstage', 70, 4);
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE LOTE INGRESSO --
+INSERT INTO tb_lote_ingresso (numero, quantidade, valor, data_inicio_venda, 
+								disponibilidade, id_setor, id_tipo)
+VALUES
+('1', '100', '300.00', '2025-12-01', TRUE, '7', '1');
+-----------------------------------------------------------------------------------------------------------------------------
+-- INSERT DE TIPO DE INGRESSO --
+INSERT INTO tb_tipo_ingresso (tipo) VALUES
+('Ingresso Geral'),
+('Ingresso VIP'),
+('Experiência Premium');
+
+select * from tb_tipo_ingresso;
+select * from tb_lote_ingresso;
 select * from tb_participante;
 select * from tb_organizador;
-select * from tb_uf;
+select * from tb_evento;
+select * from tb_setor;
+select * from tb_uf ORDER BY id;
 select * from tb_endereco_organizador;
+select * from tb_endereco_participante;
+select * from tb_endereco_evento;
+select * from tb_categoria;
