1cea5f776423c69b2d1f5cf32a82b38d5566592c
BRENO OLIVEIRA ASSIS REIS <25133656@cfp127aluno.local>
2025-12-03 11:28:23 -0300

    style: tela de cadastro

---
+++ inicial.js
@@ -0,0 +1,9 @@
+'use strict'
+
+document.getElementById('participante').onclick = () =>{
+    window.location.href = './Front-End/pages/Participante/login/index.html'
+}
+
+document.getElementById('organizador').onclick = () =>{
+    window.location.href = './Front-End/pages/Organizador/login/index.html'
+}
