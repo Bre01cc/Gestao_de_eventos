8efeaf26361ab9777d95171b33f0586839b132a2
Enzo Carrilho <enzocarrilho25@gmail.com>
2025-12-13 01:22:34 -0300

    feat: iniciando integracao de logins

--- Front-End/pages/Organizador/login/login.js
+++ Front-End/pages/Organizador/login/login.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { lerOrganizador } from "../fetch_organizador.js"
+
 const mensagemErro = document.getElementById('mensagem-erro')
 mensagemErro.classList.add('disable');
 
@@ -12,6 +14,32 @@ document.getElementById('acessar').addEventListener('click', () => {
     mensagemErro.classList.remove('disable');
     mensagemErro.classList.add('active');
 })
+
+
+async function verificarLogin() {
+    const meioDeAcesso = document.getElementById('meio-de-acesso')
+    const senha = document.getElementById('senha')
+
+    const listarOrganizadores = await lerOrganizador()
+    const organizadores = listarOrganizadores.items
+    console.log(organizadores)
+
+    
+    const verifica = organizadores.find(organizador => 
+    (organizador.cnpj === meioDeAcesso || organizador.email === meioDeAcesso) &&
+    organizador.senha === senha )
+
+    return verifica
+
+}
+
+const buttonAcesar = document.querySelector('.acessar')
+buttonAcesar.addEventListener('click', async () => {
+    const verificar = await verificarLogin()
+    if(verificar != undefined || verificar != null)
+        return verificar
+})
+
 
 
 
--- Front-End/pages/Participante/login/login.js
+++ Front-End/pages/Participante/login/login.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { lerEnderecos } from "../fetch_endereco_participante.js"
+
 const mensagemErro = document.getElementById('mensagem-erro')
 mensagemErro.classList.add('disable');
 
@@ -12,6 +14,21 @@ document.getElementById('acessar').addEventListener('click', () => {
     mensagemErro.classList.remove('disable');
     mensagemErro.classList.add('active');
 })
+
+async function verificarLogin() {
+    const meioDeAcesso = document.getElementById('meio-de-acesso')
+    const senha = document.getElementById('senha')
+
+    const listarParticipantes = await lerEnderecos()
+    const participantes = listarParticipantes.items
+    console.log(participantes)
+   
+    
+}
+
+verificarLogin()
+
+
 
 
 
